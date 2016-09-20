85427eaebd5b51645e6400c582ba942182399206
Roland Vrignon <roland.vrignon@gmail.com>
2016-09-20 23:19:31 +0200

    Hover on Galerie

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
 		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
 		<script src="http://malsup.github.com/jquery.cycle2.js"></script>
 		<link rel="stylesheet" href="Bootstrap/css/bootstrap.min.css">
+		
 
 
 		<title>IIM - Leonard de Vinci</title>
@@ -65,25 +66,79 @@
 				<img class="displayed" src="./images/image2.jpg" alt="leonard2"/>
 				<img class="displayed" src="./images/image3.jpg" alt="ordinateurs"/>
 				<img class="displayed" src="./images/image4.jpg" alt="amphitêatre"/>
+				<div class="texte">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque recusandae nisi fuga pariatur, voluptatibus tempora enim asperiores maxime quae voluptates. Dolor eligendi, iure numquam, nulla libero reiciendis autem quasi id.</div>
 			</div>
 			<br>
 		</div>
-		<p>
-			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta tenetur illum minus quos architecto exercitationem temporibus maxime sint repellat. Voluptas suscipit quas sequi nulla deserunt eaque adipisci inventore maiores culpa.
-		</p>
+		<br><br>
+
+		<h1> LES AXES </h1>
 	<br>
 	<br>
 
 	<div class="gallery">
-			<?php
+	    <ul id="da-thumbs" class="da-thumbs">
+			<section>
+			<div class="row">
+				<div class="col-md-6 col-xl-6">
+					<li>
+						<a href="http://dribbble.com/shots/502538-Natalie-Justin-Cleaning">
+							<img src="images/web.jpg" />
+							<div><span>Web & e-buisness<br>
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque cumque, modi dolorum odit minima, hic voluptates suscipit voluptas vel eius esse quam dignissimos fugit deleniti rerum officia asperiores explicabo nesciunt.</span></div>
+						</a>
+					</li>
+				</div>
+				<div class="col-md-6 col-xl-6">	
 
-			echo '<div class="titre"><h1> Les Différents axes </h1></div>';
+					<li>
+						<a href="http://dribbble.com/shots/502538-Natalie-Justin-Cleaning">
+							<img src="images/comd.jpg" />
+							<div><span>Communication digitale<br>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis repellendus voluptates ab autem amet numquam architecto rerum minus sint culpa officia ad perspiciatis, voluptate distinctio saepe quae quis dolorem velit!</span></div>
+						</a>
+					</li>
+				</div>
+					
+				<div class="row">
+				<div class="col-md-6 col-xl-6">	
+					<li>
+						<a href="http://dribbble.com/shots/502538-Natalie-Justin-Cleaning">
+							<img src="images/cv.jpg" />
+							<div><span>Communication visuelle<br>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet laboriosam, soluta quam quae, ab, culpa nihil accusamus autem fugiat doloremque deleniti ex sapiente. Voluptate sunt enim, sit iusto, ipsa blanditiis!</span></div>
+						</a>
+					</li>
 
-			include 'tableau.php';
-			foreach ($axe as $key => $value){ 
-			echo '<img src="'.$value['img'].'"/>';
-			}
-			?>
+				</div>
+				<div class="col-md-6 col-xl-6">		
+					<li>
+						<a href="http://dribbble.com/shots/502538-Natalie-Justin-Cleaning">
+							<img src="images/design.jpg" />
+							<div><span>Design Interactif<br>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut maiores nostrum ipsum sit ipsa. Autem deserunt eveniet, magnam, ducimus nihil voluptates rerum quasi accusamus odio, pariatur velit saepe explicabo quae!</span></div>
+						</a>
+					</li>
+				</div>
+				
+				<div class="row">
+				<div class="col-md-6 col-xl-6">	
+
+					<li>
+						<a href="http://dribbble.com/shots/502538-Natalie-Justin-Cleaning">
+							<img src="images/Jeux.jpg" />
+							<div><span>Jeux Vidéo<br>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem magnam accusantium corrupti earum ad. Sint placeat nisi voluptatum a ea eveniet est rem molestiae perferendis animi, qui nulla ut laudantium.</span></div>
+						</a>
+					</li>
+				</div>
+				<div class="col-md-6 col-xl-6">		
+					<li>
+
+						<a href="http://dribbble.com/shots/502538-Natalie-Justin-Cleaning">
+							<img src="images/cinéma.jpg" />
+							<div><span>Cinéma d'animation<br>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit autem sapiente consectetur, pariatur doloremque, provident accusantium facilis ratione molestias suscipit veniam fugit ut delectus animi, asperiores rem voluptatem maxime omnis.</span></div>
+						</a>
+					</li>
+				</div>	
+					</section>
+
 	</div>		
 	</body>
 </html>

c5990d9b69a39ea32f09370d8dc2e8823b4d5b00
Joshua Tabet <joshua@tabetozor.com>
2016-09-21 18:37:44 +0200

    change navbar

--- index.php
+++ index.php
@@ -1,141 +1,141 @@
-<!DOCTYPE html>
-<html lang="en">
-	<header>
-		<meta charset="UTF-8">
-		<link rel="stylesheet" type="text/css" href="CSS/style.css">
-		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
-		<script src="http://malsup.github.com/jquery.cycle2.js"></script>
-		<link rel="stylesheet" href="Bootstrap/css/bootstrap.min.css">
-		
+	<!DOCTYPE html>
+	<html lang="en">
+		<header>
+			<meta charset="UTF-8">
+			<link rel="stylesheet" type="text/css" href="CSS/style.css">
+			<script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+			<script src="http://malsup.github.com/jquery.cycle2.js"></script>
+			<link rel="stylesheet" href="Bootstrap/css/bootstrap.min.css">
+			
 
 
-		<title>IIM - Leonard de Vinci</title>
+			<title>IIM - Leonard de Vinci</title>
 
 
-<div id="background">
-	<div class="navbar navbar-fixed-top">
-  		<nav class="navbar-inner header">
-      		<div class="brand">
-      		<img class="logo" src="images/logo.png" height="80px"/>
-			      <ul class="nav pull-right">
+	<div id="background">
+		<div class="navbar navbar-fixed-top">
+	  		<nav class="navbar-inner header">
+	      		<div class="brand">
+	      		<img class="logo" src="images/logo.png" height="80px"/>
+				      <ul class="nav pull-right">
 
-			      	<li>
-		          	<a href="index.php" class="nav-link"> 
-		            	ACCUEIL
-		          	</a>
-		        </li>
-					        <li>
-					          	<a href="vie_etudiante.html" class="nav-link">
-					            	VIE ETUDIANTE
-					          	</a>
-					        </li>
+				      	<li>
+			          	<a href="index.php" class="nav-link"> 
+			            	ACCUEIL
+			          	</a>
+			        </li>
+						        <li>
+						          	<a href="vie_etudiante.html" class="nav-link">
+						            	VIE ETUDIANTE
+						          	</a>
+						        </li>
 
-					        <li>
-					          	<a href="ecole.html" class="nav-link">
-					            	l'ECOLE
-					          	</a>
-					        </li>
+						        <li>
+						          	<a href="ecole.html" class="nav-link">
+						            	l'ECOLE
+						          	</a>
+						        </li>
 
-			      </ul>
-      		</div>
-     		</nav> 
-    	</div>  
-    </div>
-<!--/cloud footer :)
--->
+				      </ul>
+	      		</div>
+	     		</nav> 
+	    	</div>  
+	    </div>
+	<!--/cloud footer :)
+	-->
 
-<br/>
-<br/>
-<br/>
-<br/>
+	<br/>
+	<br/>
+	<br/>
+	<br/>
 
 
 
-	</header>
+		</header>
 
-	<body>
-			<div class="cycle-slideshow">
-				<img class="displayed" src="./images/image1.jpg" alt="leonard" style="min-width: 100%; background-size: cover" />
-				<img class="displayed" src="./images/image2.jpg" alt="leonard2" style="min-width: 100%; background-size: cover"/>
-				<img class="displayed" src="./images/image3.jpg" alt="ordinateurs" style="min-width: 100%"/>
-				<img class="displayed" src="./images/image4.jpg" alt="amphitêatre" style="min-width: 100%"/>
-				
-			</div>
-			<br>
-		</div>
-		<br><br>
-
-		<h1> LES AXES </h1>
-	<br>
-	<br>
-
-	<div class="gallery">
-	    <ul id="da-thumbs" class="da-thumbs">
-			<section>
-			<div class="row">
-				<div class="col-md-6 col-xl-6">
-					<li>
-						<a href="http://dribbble.com/shots/502538-Natalie-Justin-Cleaning">
-							<img src="images/web.jpg" />
-							<div><span>Web & e-buisness<br>
-							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque cumque, modi dolorum odit minima, hic voluptates suscipit voluptas vel eius esse quam dignissimos fugit deleniti rerum officia asperiores explicabo nesciunt.</span></div>
-						</a>
-					</li>
-				</div>
-				<div class="col-md-6 col-xl-6">	
-
-					<li>
-						<a href="http://dribbble.com/shots/502538-Natalie-Justin-Cleaning">
-							<img src="images/comd.jpg" />
-							<div><span>Communication digitale<br>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis repellendus voluptates ab autem amet numquam architecto rerum minus sint culpa officia ad perspiciatis, voluptate distinctio saepe quae quis dolorem velit!</span></div>
-						</a>
-					</li>
-				</div>
+		<body>
+				<div class="cycle-slideshow">
+					<img class="displayed" src="./images/image1.jpg" alt="leonard" style="min-width: 100%; background-size: cover" />
+					<img class="displayed" src="./images/image2.jpg" alt="leonard2" style="min-width: 100%; background-size: cover"/>
+					<img class="displayed" src="./images/image3.jpg" alt="ordinateurs" style="min-width: 100%"/>
+					<img class="displayed" src="./images/image4.jpg" alt="amphitêatre" style="min-width: 100%"/>
 					
+				</div>
+				<br>
+			</div>
+			<br><br>
+
+			<h1> LES AXES </h1>
+		<br>
+		<br>
+
+		<div class="gallery">
+		    <ul id="da-thumbs" class="da-thumbs">
+				<section>
 				<div class="row">
-				<div class="col-md-6 col-xl-6">	
-					<li>
-						<a href="http://dribbble.com/shots/502538-Natalie-Justin-Cleaning">
-							<img src="images/cv.jpg" />
-							<div><span>Communication visuelle<br>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet laboriosam, soluta quam quae, ab, culpa nihil accusamus autem fugiat doloremque deleniti ex sapiente. Voluptate sunt enim, sit iusto, ipsa blanditiis!</span></div>
-						</a>
-					</li>
+					<div class="col-md-6 col-xl-6">
+						<li>
+							<a href="http://dribbble.com/shots/502538-Natalie-Justin-Cleaning">
+								<img src="images/web.jpg" />
+								<div><span>Web & e-buisness<br>
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque cumque, modi dolorum odit minima, hic voluptates suscipit voluptas vel eius esse quam dignissimos fugit deleniti rerum officia asperiores explicabo nesciunt.</span></div>
+							</a>
+						</li>
+					</div>
+					<div class="col-md-6 col-xl-6">	
 
-				</div>
-				<div class="col-md-6 col-xl-6">		
-					<li>
-						<a href="http://dribbble.com/shots/502538-Natalie-Justin-Cleaning">
-							<img src="images/design.jpg" />
-							<div><span>Design Interactif<br>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut maiores nostrum ipsum sit ipsa. Autem deserunt eveniet, magnam, ducimus nihil voluptates rerum quasi accusamus odio, pariatur velit saepe explicabo quae!</span></div>
-						</a>
-					</li>
-				</div>
-				
-				<div class="row">
-				<div class="col-md-6 col-xl-6">	
+						<li>
+							<a href="http://dribbble.com/shots/502538-Natalie-Justin-Cleaning">
+								<img src="images/comd.jpg" />
+								<div><span>Communication digitale<br>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis repellendus voluptates ab autem amet numquam architecto rerum minus sint culpa officia ad perspiciatis, voluptate distinctio saepe quae quis dolorem velit!</span></div>
+							</a>
+						</li>
+					</div>
+						
+					<div class="row">
+					<div class="col-md-6 col-xl-6">	
+						<li>
+							<a href="http://dribbble.com/shots/502538-Natalie-Justin-Cleaning">
+								<img src="images/cv.jpg" />
+								<div><span>Communication visuelle<br>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet laboriosam, soluta quam quae, ab, culpa nihil accusamus autem fugiat doloremque deleniti ex sapiente. Voluptate sunt enim, sit iusto, ipsa blanditiis!</span></div>
+							</a>
+						</li>
 
-					<li>
-						<a href="http://dribbble.com/shots/502538-Natalie-Justin-Cleaning">
-							<img src="images/Jeux.jpg" />
-							<div><span>Jeux Vidéo<br>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem magnam accusantium corrupti earum ad. Sint placeat nisi voluptatum a ea eveniet est rem molestiae perferendis animi, qui nulla ut laudantium.</span></div>
-						</a>
-					</li>
-				</div>
-				<div class="col-md-6 col-xl-6">		
-					<li>
+					</div>
+					<div class="col-md-6 col-xl-6">		
+						<li>
+							<a href="http://dribbble.com/shots/502538-Natalie-Justin-Cleaning">
+								<img src="images/design.jpg" />
+								<div><span>Design Interactif<br>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut maiores nostrum ipsum sit ipsa. Autem deserunt eveniet, magnam, ducimus nihil voluptates rerum quasi accusamus odio, pariatur velit saepe explicabo quae!</span></div>
+							</a>
+						</li>
+					</div>
+					
+					<div class="row">
+					<div class="col-md-6 col-xl-6">	
 
-						<a href="http://dribbble.com/shots/502538-Natalie-Justin-Cleaning">
-							<img src="images/cinema.jpg" />
-							<div><span>Cinéma d'animation<br>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit autem sapiente consectetur, pariatur doloremque, provident accusantium facilis ratione molestias suscipit veniam fugit ut delectus animi, asperiores rem voluptatem maxime omnis.</span></div>
-						</a>
-					</li>
-				</div>	
-					</section>
+						<li>
+							<a href="http://dribbble.com/shots/502538-Natalie-Justin-Cleaning">
+								<img src="images/Jeux.jpg" />
+								<div><span>Jeux Vidéo<br>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem magnam accusantium corrupti earum ad. Sint placeat nisi voluptatum a ea eveniet est rem molestiae perferendis animi, qui nulla ut laudantium.</span></div>
+							</a>
+						</li>
+					</div>
+					<div class="col-md-6 col-xl-6">		
+						<li>
 
-	</div>	
- <footer>
- 	<a href="https://www.facebook.com/Institut.Internet.Multimedia/?fref=ts"><img src="images/facebooklogo.png" width="3%"></a>
-		<a href="https://twitter.com/IIMparis"><img src="images/twitter2.png" width="3%">
- </footer>	
-	</body>
-</html>
+							<a href="http://dribbble.com/shots/502538-Natalie-Justin-Cleaning">
+								<img src="images/cinema.jpg" />
+								<div><span>Cinéma d'animation<br>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit autem sapiente consectetur, pariatur doloremque, provident accusantium facilis ratione molestias suscipit veniam fugit ut delectus animi, asperiores rem voluptatem maxime omnis.</span></div>
+							</a>
+						</li>
+					</div>	
+						</section>
+
+		</div>	
+	 <footer>
+	 	<a href="https://www.facebook.com/Institut.Internet.Multimedia/?fref=ts"><img src="images/facebooklogo.png" width="3%"></a>
+			<a href="https://twitter.com/IIMparis"><img src="images/twitter2.png" width="3%">
+	 </footer>	
+		</body>
+	</html>

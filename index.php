4b572240a92fe525b258ad7a60a75c5d4c206baf
Joshua Tabet <joshua@tabetozor.com>
2016-09-23 19:07:00 +0200

    add container and supp <br/>

--- index.php
+++ index.php
@@ -1,119 +1,125 @@
-<!DOCTYPE html>
+	<!DOCTYPE html>
 
-<html lang="en">
+	<html>
+	<head>
+		<meta charset="UTF-8">
+				<link rel="stylesheet" type="text/css" href="CSS/style.css">
+				<script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+				<script src="http://malsup.github.com/jquery.cycle2.js"></script>
+				<link rel="stylesheet" href="Bootstrap/css/bootstrap.min.css">
 
-	<title>IIM - Leonard de Vinci</title>
+		<title>IIM - Leonard de Vinci</title>
+		</head>
 
-	<body class="news">
-		<header>
-		  	<meta charset="UTF-8">
-			<link rel="stylesheet" type="text/css" href="CSS/style.css">
-			<script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
-			<script src="http://malsup.github.com/jquery.cycle2.js"></script>
-			<link rel="stylesheet" href="Bootstrap/css/bootstrap.min.css">
+		<body class="news">
+			<header>
+			  
 
-		    <div class="nav">
+			    <div class="nav">
 
-		      <ul>
-		        <img src="images/logoiim.png" alt="logo" width="5%">
-		        <li class="home"><a href="index.php">ACCUEIL</a></li>
-		        <li class="tutorials"><a href="ecole.html">ECOLE</a></li>
-		        <li class="about"><a href="vie_etudiante.html">VIE ETUDIANTE</a></li>
-		      </ul>
-		    </div>
-		  </header>
+			      <ul>
+			        <img src="images/logoiim.png" alt="logo" width="5%">
+			        <li class="home"><a href="index.php">ACCUEIL</a></li>
+			        <li class="tutorials"><a href="ecole.html">ECOLE</a></li>
+			        <li class="about"><a href="vie_etudiante.html">VIE ETUDIANTE</a></li>
+			      </ul>
+			    </div>
+			  </header>
 
 
-				<div class="cycle-slideshow">
-					<img class="displayed" src="./images/image1.jpg" alt="leonard" style="min-width: 100%; background-size: cover" />
-					<img class="displayed" src="./images/image2.jpg" alt="leonard2" style="min-width: 100%; background-size: cover"/>
-					<img class="displayed" src="./images/image3.jpg" alt="ordinateurs" style="min-width: 100%"/>
-					<img class="displayed" src="./images/image4.jpg" alt="amphitêatre" style="min-width: 100%"/>
-					
+					<div class="cycle-slideshow">
+						<img class="displayed" src="./images/image1.jpg" alt="leonard" style="min-width: 100%; background-size: cover" />
+						<img class="displayed" src="./images/image2.jpg" alt="leonard2" style="min-width: 100%; background-size: cover"/>
+						<img class="displayed" src="./images/image3.jpg" alt="ordinateurs" style="min-width: 100%"/>
+						<img class="displayed" src="./images/image4.jpg" alt="amphitêatre" style="min-width: 100%"/>
+						
+					</div>
 				</div>
+				
+
+				<h1> LES AXES </h1>
+			
+				<div class="container">
+			<div class="gallery">
+			    <ul id="da-thumbs" class="da-thumbs">
+					<section>
+					
+					<div class="row">
+						<div class="col-md-6 col-xl-6">
+							<li>
+								<a href="http://dribbble.com/shots/502538-Natalie-Justin-Cleaning">
+									<img src="images/web.jpg" />
+									<div><span>Web & e-buisness<br>
+									En moins de 20 ans, Internet a révolutionné l’économie mondiale et notre manière de consommer ! Ce secteur, créateur d’emplois et en plein développement, offre de réelles perspectives de carrière chez l’annonceur et en agence pour des profils techniques, stratégiques.</span></div>
+								</a>
+							</li>
+						</div>
+						<div class="col-md-6 col-xl-6">	
+
+							<li>
+								<a href="http://dribbble.com/shots/502538-Natalie-Justin-Cleaning">
+									<img src="images/comd.jpg" />
+									<div><span>Communication digitale<br>En intégrant les pratiques d’échanges les professionnels des métiers du commerce, du marketing, des technologies et de la communication s’adaptent, créant ainsi de nouvelles méthodes de travail nécessaires à la maîtrise du foisonnement des contenus des supports.</span></div>
+								</a>
+							</li>
+						</div>
+						</div>
+							
+						<div class="row">
+						<div class="col-md-6 col-xl-6">	
+							<li>
+								<a href="http://dribbble.com/shots/502538-Natalie-Justin-Cleaning">
+									<img src="images/cv.jpg" />
+									<div><span>Communication visuelle<br>Sites web, affiches, journaux, applications mobiles, spots publicitaires, serious games.les codes visuels, leurs cycles de vie de plus en plus courts, les exposent à une obsolescence rapide et à une usure des messages qui impose, tant chez les prestataires que chez les clients.</span></div>
+								</a>
+							</li>
+
+						</div>
+
+						<div class="col-md-6 col-xl-6">		
+							<li>
+								<a href="http://dribbble.com/shots/502538-Natalie-Justin-Cleaning">
+									<img src="images/design.jpg" />
+									<div><span>Design Interactif<br>Depuis quelques années, notre relation avec le numérique s’est profondément transformée. À l’origine, figées sur un ordinateur de bureau.Le design interactif est l’activité créatrice dédiée à la conception de ces nouveaux produits et services hybrides où l’utilisateur tient la place centrale.</span></div>
+								</a>
+							</li>
+						</div>
+						</div>
+						
+						<div class="row">
+						<div class="col-md-6 col-xl-6">	
+
+							<li>
+								<a href="http://dribbble.com/shots/502538-Natalie-Justin-Cleaning">
+									<img src="images/Jeux.jpg" />
+									<div><span>Jeux Vidéo<br>Le secteur du jeu vidéo est un secteur en plein essor, avec une croissance et un chiffre d’affaires supérieurs à celui du cinéma. Avec l’apparition de nouveaux médias et supports (réseaux sociaux, tablettes, Smartphones), de nouveaux moyens de diffusion (PS network, XboxLive, TV interactive, Cloud gaming…) et le développement de nouveaux modèles économiques (free-to-play, micro-paiements…) le monde du jeu vidéo évolue constamment.</span></div>
+								</a>
+							</li>
+						</div>
+						<div class="col-md-6 col-xl-6">		
+							<li>
+
+								<a href="http://dribbble.com/shots/502538-Natalie-Justin-Cleaning">
+									<img src="images/cinema.jpg" />
+									<div><span>Cinéma d'animation<br>Les entreprises du secteur de l’image de synthèse et des effets spéciaux numériques produisent des images de plus en plus étonnantes grâce aux innovations techniques. De nombreuses entreprises spécialisées, en France et à l’étranger, fabriquent des produits aussi divers que les longs-métrages d’animation ou d’effets spéciaux, les publicités, les séries d’animation, les documentaires incluant des effets spéciaux ou les jeux vidéo…</span></div>
+								</a>
+							</li>
+						</div>	
+						</div>
+						</section>
+						</ul>
+			</div>	
 			</div>
 			
 
-			<h1> LES AXES </h1>
-		
-
-		<div class="gallery">
-		    <ul id="da-thumbs" class="da-thumbs">
-				<section>
-				<div class="row">
-					<div class="col-md-6 col-xl-6">
-						<li>
-							<a href="http://dribbble.com/shots/502538-Natalie-Justin-Cleaning">
-								<img src="images/web.jpg" />
-								<div><span>Web & e-buisness<br>
-								En moins de 20 ans, Internet a révolutionné l’économie mondiale et notre manière de consommer ! Ce secteur, créateur d’emplois et en plein développement, offre de réelles perspectives de carrière chez l’annonceur et en agence pour des profils techniques, stratégiques.</span></div>
-							</a>
-						</li>
-					</div>
-					<div class="col-md-6 col-xl-6">	
-
-						<li>
-							<a href="http://dribbble.com/shots/502538-Natalie-Justin-Cleaning">
-								<img src="images/comd.jpg" />
-								<div><span>Communication digitale<br>En intégrant les pratiques d’échanges les professionnels des métiers du commerce, du marketing, des technologies et de la communication s’adaptent, créant ainsi de nouvelles méthodes de travail nécessaires à la maîtrise du foisonnement des contenus des supports.</span></div>
-							</a>
-						</li>
-					</div>
-					</div>
-						
-					<div class="row">
-					<div class="col-md-6 col-xl-6">	
-						<li>
-							<a href="http://dribbble.com/shots/502538-Natalie-Justin-Cleaning">
-								<img src="images/cv.jpg" />
-								<div><span>Communication visuelle<br>Sites web, affiches, journaux, applications mobiles, spots publicitaires, serious games.les codes visuels, leurs cycles de vie de plus en plus courts, les exposent à une obsolescence rapide et à une usure des messages qui impose, tant chez les prestataires que chez les clients.</span></div>
-							</a>
-						</li>
-
-					</div>
-
-					<div class="col-md-6 col-xl-6">		
-						<li>
-							<a href="http://dribbble.com/shots/502538-Natalie-Justin-Cleaning">
-								<img src="images/design.jpg" />
-								<div><span>Design Interactif<br>Depuis quelques années, notre relation avec le numérique s’est profondément transformée. À l’origine, figées sur un ordinateur de bureau.Le design interactif est l’activité créatrice dédiée à la conception de ces nouveaux produits et services hybrides où l’utilisateur tient la place centrale.</span></div>
-							</a>
-						</li>
-					</div>
-					</div>
-					
-					<div class="row">
-					<div class="col-md-6 col-xl-6">	
-
-						<li>
-							<a href="http://dribbble.com/shots/502538-Natalie-Justin-Cleaning">
-								<img src="images/Jeux.jpg" />
-								<div><span>Jeux Vidéo<br>Le secteur du jeu vidéo est un secteur en plein essor, avec une croissance et un chiffre d’affaires supérieurs à celui du cinéma. Avec l’apparition de nouveaux médias et supports (réseaux sociaux, tablettes, Smartphones), de nouveaux moyens de diffusion (PS network, XboxLive, TV interactive, Cloud gaming…) et le développement de nouveaux modèles économiques (free-to-play, micro-paiements…) le monde du jeu vidéo évolue constamment.</span></div>
-							</a>
-						</li>
-					</div>
-					<div class="col-md-6 col-xl-6">		
-						<li>
-
-							<a href="http://dribbble.com/shots/502538-Natalie-Justin-Cleaning">
-								<img src="images/cinema.jpg" />
-								<div><span>Cinéma d'animation<br>Les entreprises du secteur de l’image de synthèse et des effets spéciaux numériques produisent des images de plus en plus étonnantes grâce aux innovations techniques. De nombreuses entreprises spécialisées, en France et à l’étranger, fabriquent des produits aussi divers que les longs-métrages d’animation ou d’effets spéciaux, les publicités, les séries d’animation, les documentaires incluant des effets spéciaux ou les jeux vidéo…</span></div>
-							</a>
-						</li>
-					</div>	
-					</div>
-					</section>
-					</ul>
-		</div>	
 
 
+	</body>
 
-</body>
-
-	 <footer>
-	 	<a href="https://www.facebook.com/Institut.Internet.Multimedia/?fref=ts"><img src="images/facebook_black.png" width="2%"></a>
-		<a href="https://twitter.com/IIMparis"><img src="images/twitter_black.png" width="2%"></a>
-	 </footer>	
+		 <footer>
+		 	<a href="https://www.facebook.com/Institut.Internet.Multimedia/?fref=ts"><img src="images/facebook_black.png" width="2%"></a>
+			<a href="https://twitter.com/IIMparis"><img src="images/twitter_black.png" width="2%"></a>
+		 </footer>	
 
 
-</html>
+	</html>
